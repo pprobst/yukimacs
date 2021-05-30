b82c9d57e5060a386c190d6d7697e0ce94f35d95
pprobst <pprobst@insiberia.net>
2021-05-30 18:05:57 -0300

    Better performance I hope

--- init.el
+++ init.el
@@ -1,8 +1,23 @@
 ;;; init.el
 
-;;; This fixed garbage collection makes emacs start up faster ;;;;;;;
-(setq gc-cons-threshold 402653184
-      gc-cons-percentage 0.6)
+;;; Better startup/performance.
+;; https://emacs-lsp.github.io/lsp-mode/page/performance/
+(setq gc-cons-threshold 100000000
+      read-process-output-max (* 1024 1024))
+
+;; https://www.masteringemacs.org/article/speed-up-emacs-libjansson-native-elisp-compilation
+(if (and (fboundp 'native-comp-available-p)
+         (native-comp-available-p))
+    (setq comp-deferred-compilation t
+          package-native-compile t)
+  (message "Native complation is *not* available, lsp performance will suffer..."))
+
+(unless (functionp 'json-serialize)
+  (message "Native JSON is *not* available, lsp performance will suffer..."))
+
+;; do not steal focus while doing async compilations
+(setq warning-suppress-types '((comp)))
+
 
 (defvar startup/file-name-handler-alist file-name-handler-alist)
 (setq file-name-handler-alist nil)
