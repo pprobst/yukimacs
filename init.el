19655be4a6a82477c9b71fb13fdc9f59392f4288
pprobst <pprobst@insiberia.net>
2021-07-27 17:17:37 -0300

    Update config with sublimity and go-mode

--- init.el
+++ init.el
@@ -14,7 +14,10 @@
 (unless (functionp 'json-serialize)
   (message "Native JSON is *not* available, lsp performance will suffer..."))
 
-;; Do not steal focus while doing async compilations
+;; Increase the amount of data which Emacs reads from the process. 
+(setq read-process-output-max (* 1024 1024)) ;; 1mb
+
+;; Do not steal focus while doing async compilations.
 (setq warning-suppress-types '((comp)))
 
 (setq comp-deferred-compilation t)
@@ -75,12 +78,6 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "#0F0F0F" :foreground "#D2D5D6" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "BE5N" :family "Iosevka Comfy")))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -88,9 +85,12 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(yukimacs))
  '(custom-safe-themes
-   '("c493feac62227a461d56be5798cb188d4f1da0cfa8c0406f499d1422adb9baa2" default))
- '(org-export-backends '(ascii beamer html icalendar latex man md odt))
- '(package-selected-packages
-   '(exec-path-from-shell emojify toc-org cmake-mode guess-language hl-todo highlight-numbers company-reftex company-math company-auctex auctex marginalia lsp-haskell haskell-mode evil-org org-superstar treemacs-projectile evil-collection treemacs-all-the-icons all-the-icons-dired all-the-icons evil-nerd-commenter ccls no-littering ctrlf orderless popup-kill-ring helpful company-lsp lsp-python-ms flycheck-rust rustic powerline treemacs-magit magit slime-company slime company-jedi company-irony company-c-headers flycheck-clang-analyzer dap-mode auto-yasnippet yasnippet-snippets lsp-ui yasnippet company dashboard treemacs-evil treemacs rainbow-delimiters rainbow-mode switch-window avy ido-vertical-mode auto-package-update beacon evil which-key diminish htmlize doom-themes spacemacs-theme use-package)))
+   '("f16fcabba9ca65097d2373c32de76a939e1d8d2bf62521b643f5e81c68ede694" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "UKWN" :family "Iosevka Comfy")))))
 
 ;;; init.el ends here
